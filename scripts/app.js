a59f2a701bcb4cd11dfe1a6013dbcf9cd8306d88
Leonardo Sodré <LeonardoSodre25@outlook.com>
2022-02-10 21:10:55 -0300

    em andamento

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,13 @@ const requestCharactersAPI = () => {
                 json.results.map(results => [
 
                     containerMain.innerHTML+= `
-                        <div class="container-character"><img src=${results.image}></div>
+                        <div class="container-character">
+                            <div class="container-img-character"><img src=${results.image}></div>
+                            <h1>${results.name}</h1>
+                            <h2>${results.species}</h2>
+                            <h3>${results.status}</h3>
+                            <h4>${results.gender}</h4>
+                        </div>
                     `    
                 ])
 
